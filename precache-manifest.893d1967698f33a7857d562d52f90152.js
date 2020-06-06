5c72fad17b054d3caa4aa72b2c1687a0ce83287f
usamabinzubair <usamazubair@hotmail.co.uk>
2020-06-06 22:07:34 +0100

    Updates

---
+++ precache-manifest.893d1967698f33a7857d562d52f90152.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1de4368e4ad5182aa0b3277c314259",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0aec35f3dbf038c4b8e1",
+    "url": "/portfolio/static/css/main.63d1a3d0.chunk.css"
+  },
+  {
+    "revision": "1d30827bad1fcfdef92b",
+    "url": "/portfolio/static/js/2.176dfa50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.176dfa50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aec35f3dbf038c4b8e1",
+    "url": "/portfolio/static/js/main.b6727874.chunk.js"
+  },
+  {
+    "revision": "8ac7186355a06dc24344",
+    "url": "/portfolio/static/js/runtime-main.08e71136.js"
+  },
+  {
+    "revision": "d9328fe1ca683088e2bb93f0a83fe74b",
+    "url": "/portfolio/static/media/apocalypse.d9328fe1.jpg"
+  },
+  {
+    "revision": "893c9224714f3d3218a3573dd1ba3867",
+    "url": "/portfolio/static/media/dicegme.893c9224.jpg"
+  },
+  {
+    "revision": "f8037a275d9db985c77af4b4d9651539",
+    "url": "/portfolio/static/media/homeimg.f8037a27.jpg"
+  },
+  {
+    "revision": "9c8287e08f3fdc055107551e849a346e",
+    "url": "/portfolio/static/media/keycode.9c8287e0.jpg"
+  },
+  {
+    "revision": "2e1b3b0e3044b6a52837d3bcaa93e266",
+    "url": "/portfolio/static/media/profile.2e1b3b0e.jpg"
+  },
+  {
+    "revision": "dcc32f3864c5f074d504eb592ad12759",
+    "url": "/portfolio/static/media/sajidas.dcc32f38.jpg"
+  },
+  {
+    "revision": "d7912e0a340e614c69703a3bbb8a48d8",
+    "url": "/portfolio/static/media/weatherapp.d7912e0a.jpg"
+  }
+]);
